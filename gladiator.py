524f4930a5a5c61696b17ea3a05999d0989737c5
Daniel Mohammadi <daniel.mohammadi@learnet.se>
2024-10-09 15:57:12 +0200

    Version 2

--- gladiator.py
+++ gladiator.py
@@ -1,4 +1,9 @@
+import random
 namn = input("Vad är ditt namn?")
-
 print(f"Välkommen till Colosseum! Du {namn} är en gladiator som ska strida en motståndare tills en dödas.")
-
+motståndarna = ["Spartacus", "Flamma", "Priscus", "Spiculus", "Tetraites", "Commodus", "Marcus", "Aurelius", "Decimus", "Gaius", "Maximus", "Verus", "Crixus", "Carpophorus", "Antonius", "Augustus"]
+motståndaren = random.choice(motståndarna)
+attacker = ["näve", "spark"]
+spelaren_hp = 10
+fienden_hp = 8
+strid = True
